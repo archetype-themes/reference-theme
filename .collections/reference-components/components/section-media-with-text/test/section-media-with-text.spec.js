b35655838f5e2fcc305d0f517dc9c8a39646b7b5
Justin <justinbansal@gmail.com>
2024-05-02 10:58:04 -0400

    Removed cached files

--- .collections/reference-components/components/section-media-with-text/test/section-media-with-text.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('test', async ({ page }) => {
-  await page.goto('/')
-  await page.getByRole('link', { name: 'section-media-with-text' }).click()
-  await page.getByRole('link', { name: 'Watch movie' }).click()
-})
